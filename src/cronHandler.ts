b0ee970c04c92e550d6ae990f8bfa061c22d0416
FunTuan <hank85627@gmail.com>
2023-09-27 00:13:38 +0800

    🐛 Fix: 通知機制調整

--- src/cronHandler.ts
+++ src/cronHandler.ts
@@ -127,53 +127,53 @@ export const cronNotify = async (env: Bindings) => {
 
   const newJobs = currentJobs.filter((job: any) => !reminderNotifyWorkIds.includes(job.workId))
 
-  if (newJobs.length === 0) return
+  if (newJobs.length > 0) {
+    for (const key of keys) {
+      const data = await env.kv.get(key.name)
+      console.log(`notifyConfig: ${key.name}`, data)
+      if (!data) continue
+      const notifyConfig: NotifyConfig = JSON.parse(data)
 
-  for (const key of keys) {
-    const data = await env.kv.get(key.name)
-    console.log(`notifyConfig: ${key.name}`, data)
-    if (!data) continue
-    const notifyConfig: NotifyConfig = JSON.parse(data)
+      const matchedJobs = newJobs.filter((job: any) => checkConditions(job, notifyConfig.condition))
 
-    const matchedJobs = newJobs.filter((job: any) => checkConditions(job, notifyConfig.condition))
+      if (matchedJobs.length === 0) continue
 
-    if (matchedJobs.length === 0) continue
+      for (let i = 0; i < Math.min(matchedJobs.length, 10); i++) {
+        try {
+          await sendLineNotify(
+            stringifyData(matchedJobs[i].fields),
+            notifyConfig.lineNotifyToken,
+          )
+        } catch (error) {
+          // 如果被解除授權，刪除 notifyConfig
+          if (error.response?.data?.code === 401) {
+            await env.kv.delete(key.name)
+          }
+          console.log(error)
+          continue
+        }
+      }
 
-    for (let i = 0; i < Math.min(matchedJobs.length, 10); i++) {
-      try {
+      // 隨機產生 id
+      const id = Math.random().toString(36).substring(2, 15)
+      const viewUrl = `${env.BACKEND_HOST}/view/${id}`
+      await env.kv.put(`view:${id}`, JSON.stringify(matchedJobs), {
+        expirationTtl: 60 * 60 * 24 * 7, // 7 days
+      })
+
+      const systemContent = `設定其他條件 ${env.FRONTEND_HOST}\n取消通知訂閱 https://notify-bot.line.me/my/`
+
+      if (matchedJobs.length > 10) {
         await sendLineNotify(
-          stringifyData(matchedJobs[i].fields),
+          `今日符合職缺共 ${matchedJobs.length} 筆，以上只顯示前 10 筆\n${viewUrl}\n\n${systemContent}`,
           notifyConfig.lineNotifyToken,
         )
-      } catch (error) {
-        // 如果被解除授權，刪除 notifyConfig
-        if (error.response?.data?.code === 401) {
-          await env.kv.delete(key.name)
-        }
-        console.error(error)
-        continue
+      } else {
+        await sendLineNotify(
+          `今日符合職缺共 ${matchedJobs.length} 筆，以上為全部\n${viewUrl}\n\n${systemContent}`,
+          notifyConfig.lineNotifyToken,
+        )
       }
-    }
-
-    // 隨機產生 id
-    const id = Math.random().toString(36).substring(2, 15)
-    const viewUrl = `${env.BACKEND_HOST}/view/${id}`
-    await env.kv.put(`view:${id}`, JSON.stringify(matchedJobs), {
-      expirationTtl: 60 * 60 * 24 * 7, // 7 days
-    })
-
-    const systemContent = `設定其他條件 ${env.FRONTEND_HOST}\n取消通知訂閱 https://notify-bot.line.me/my/`
-
-    if (matchedJobs.length > 10) {
-      await sendLineNotify(
-        `今日符合職缺共 ${matchedJobs.length} 筆，以上只顯示前 10 筆\n${viewUrl}\n\n${systemContent}`,
-        notifyConfig.lineNotifyToken,
-      )
-    } else {
-      await sendLineNotify(
-        `今日符合職缺共 ${matchedJobs.length} 筆，以上為全部\n${viewUrl}\n\n${systemContent}`,
-        notifyConfig.lineNotifyToken,
-      )
     }
   }
 
